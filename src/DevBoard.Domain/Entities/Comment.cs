032846ba07b82b9e2601df4183897f95ea57a22e
Muhammad <Muhammad@DESKTOP-12N3Q9G>
2025-10-26 22:26:56 +0500

    Add Comment entity and update TaskItem with ImageUrl as per mantainer request

---
+++ src/DevBoard.Domain/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBoard.Domain.Entities
+{
+    public class Comment : EntityBase<Guid>
+    {
+        public string Text { get; set; }
+
+        public Guid TaskItemId { get; set; }
+        public TaskItem TaskItem { get; set; } = null!;
+    }
+}
